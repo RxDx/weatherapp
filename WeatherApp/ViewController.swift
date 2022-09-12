840feadee8691c23bfb26399cc1be88d71986cb4
Rodrigo <rodrigo@rxdx.com.br>
2022-09-12 19:20:03 -0300

    iOS Deployment Target to 10.0

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello World!"
+        return label
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        super.loadView()
+        setupView()
     }
 
-
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 }
 
